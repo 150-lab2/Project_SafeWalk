8bf876910641e18e6fd2f72b61a6f1a740a708ab
Amazingpaper01 <julianprater26@gmail.com>
2023-09-27 10:30:11 -0700

    09/26/2023

--- Project SafeWalk/src/app/app-routing.module.ts
+++ Project SafeWalk/src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  }
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  // }
 ];
 @NgModule({
   imports: [
