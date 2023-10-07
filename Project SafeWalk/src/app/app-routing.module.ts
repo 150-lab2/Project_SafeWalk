404dcb7c1904e17e3cae1b8c9c97aa1b5b159c08
Amazingpaper01 <julianprater26@gmail.com>
2023-10-07 15:46:03 -0700

    Added login Page

--- Project SafeWalk/src/app/app-routing.module.ts
+++ Project SafeWalk/src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+//import { LoginexammplePage } from './loginexample/loginexample-routing.module'; // Import your login component
+import { TabsPage } from './tabs/tabs.page'; // Import your tabs page component
+import { Router } from '@angular/router';
+
+
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'login', // Define a route for the login page
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
-  // {
-  //   path: 'login',
-  //   loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  // }
+  {
+    path: 'tabs', // Define a route for the tabs page
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+  },
+  { 
+    path: '', 
+    redirectTo: 'login', // Set the initial route to the login page
+    pathMatch: 'full' 
+  }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
@@ -18,3 +29,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
---
+++ Project SafeWalk/src/app/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    if (this.authService.login(this.username, this.password)) {
+      // Navigate to the tabs page after successful login
+      this.router.navigate(['/tabs']);
+    } else {
+      // Display an error message or handle authentication failure
+      console.log('Login failed');
+    }
+  }
+}
