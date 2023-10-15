6824d3f941be3f9ab7047975806c271f74a96193
Amazingpaper01 <julianprater26@gmail.com>
2023-10-14 22:48:59 -0700

    Added sign up modal

--- Project SafeWalk/src/app/login/login.page.ts
+++ Project SafeWalk/src/app/login/login.page.ts
@@ -3,31 +3,42 @@ import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage {
+  email: string = '';
   username: string = '';
   password: string = '';
   data: []= [];
 
-  //$observ: Observable
+  posttData: any = {email: '', password: ''};
 
-  posttData: any = {name: '', password: ''};
+  constructor(private http: HttpClient, private router: Router) { }
+  
 
-  constructor(private http: HttpClient) { }
-
-  postData() {
+  postData() {                //chekcing if login is correct
     this.http.post('http://localhost:3000/login', this.posttData).subscribe(
       (response) => {
         console.log('Data posted successfully:', response);
+        this.router.navigate(['/tabs']);              // routes to home page
       },
-      (error) => {
+      (error) => {            //prints out error if unsucessful login attempt is made.
         console.error('Error posting data:', error);
       }
     );
+  }
+
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
   }
 }
