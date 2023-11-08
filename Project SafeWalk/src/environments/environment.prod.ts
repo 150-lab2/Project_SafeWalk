9dcffc8dd3ff70feba8512672ada2f4d6c27fb44
Amazingpaper01 <julianprater26@gmail.com>
2023-11-08 15:13:08 -0800

    Google OAuth

--- Project SafeWalk/src/environments/environment.prod.ts
+++ Project SafeWalk/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyDMyfxLu7x_6If-6mmgGjhKvwlxm2umozQ",
-    authDomain: "safewalk-backend.firebaseapp.com",
-    projectId: "safewalk-backend",
-    storageBucket: "safewalk-backend.appspot.com",
-    messagingSenderId: "418974287278",
-    appId: "1:418974287278:web:fd005f633f22d3e817343f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
   production: true
 };
